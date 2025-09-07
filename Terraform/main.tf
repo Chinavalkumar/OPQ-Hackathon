014e2affe97679415e3a57793c6de6ef8f98addc
Chinavalkumar <mutturajshetty20@gmail.com>
2025-09-07 19:53:40 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,203 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+data "aws_availability_zones" "available_zones" {}
+
+resource "aws_subnet" "public_subnet_az1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet az1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet az2"
+  }
+}
+
+
+resource "aws_subnet" "private_subnet_az1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet az1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_az2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet az2"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = var.ecr_repository_name       
+  image_tag_mutability = "MUTABLE"           
+  tags = {
+
+    Project     = "my-project"
+  }
+}
+
+# Create a security group for the ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# Create the ALB
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets             = [
+    aws_subnet.public_subnet_az1.id,
+    aws_subnet.public_subnet_az2.id
+    ]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+# Create a target group
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    matcher             = "200-399"
+  }
+
+  tags = {
+    Name = "app-tg"
+  }
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.3" 
+  
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids   = [
+    aws_subnet.private_subnet_az1.id,
+    aws_subnet.private_subnet_az2.id
+   ] 
+  vpc_id          = aws_vpc.vpc.id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+  }
+}
